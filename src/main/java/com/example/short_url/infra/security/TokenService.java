93e8e9f0987bd98d1eac4e6af5ae8f6641b4c2af
Felipe Saff <luizsaff@tutanota.com>
2026-03-11 08:08:04 -0400

    chore: add TokenService

---
+++ src/main/java/com/example/short_url/infra/security/TokenService.java
@@ -0,0 +1,54 @@
+package com.example.short_url.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.example.short_url.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("login-service")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("login-service")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime
+                .now()
+                .plusHours(1)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+}
